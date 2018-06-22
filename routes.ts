065733e2a22f8cf85d05ee4480361d261b8b12a3
murtazasmart <murtazasmart@hotmail.com>
2018-06-22 09:27:23 +0530

    Fixing cors

--- routes.ts
+++ routes.ts
@@ -4,11 +4,16 @@
 
 import ping from './api/ping'
 import view from './api/view'
+import * as cors from "cors"
 import user from './api/user'
 import transaction from './api/transaction'
 import * as express from 'express'
 
 const routes = (app: express.Application): void => {
+
+  // Use CORS
+  app.use(cors());
+  
   // Insert routes below
   app.use('/api/ping', ping);
   app.use('/api/views', view);
